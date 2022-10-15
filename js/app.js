5cfb644e7fedc98ebc24cd356ae8c89c26e796f5
Meisi Rallin <maximiliano.rallin@gmail.com>
2022-10-15 16:20:40 -0300

    se realiza JS alternativo al proyecto original para realizar la entrega

--- js/app.js
+++ js/app.js
@@ -7,19 +7,22 @@ let precioBebidas
 //genero una funcion con condicional
 
 function intro() {
-    if (nombre != "" && apellido != "" && nombre != null && apellido != null) {
         alert ("Bienvenido a nuestro simulador de carrito, "+nombre+" "+apellido+"!")
         numeroBebidas = parseInt (prompt (nombre +","+" ingrese el numero de bebidas que desea comprar"))
         alert (nombre +", El total a pagar por sus bebidas es de: "+"$"+calcPrecio ())
-    }
-    else {
-        alert ("No ingresaste ningun dato")
-    }
 }
 
-nombre = (prompt ("Ingrese su nombre"))
-apellido = (prompt ("Ingrese su apellido"))
+function promptVariable(nombre_del_parametro) {
+    let result = null
+    while (result == null || result == "") {
+        result = prompt("Ingrese su " + nombre_del_parametro)
+    }
 
+    return result
+}
+
+nombre = promptVariable("nombre")
+apellido = promptVariable("apellido")
 //invoco la funcion
 
 intro ();
